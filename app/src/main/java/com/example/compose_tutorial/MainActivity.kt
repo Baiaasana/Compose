1a45a0279730c1b645b4e7c96606fbc657b0f278
b.asanidze <b.asanidze@mm.ge>
2023-11-14 19:16:40 +0400

    initial commit

--- app/src/main/java/com/example/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/compose_tutorial/MainActivity.kt
@@ -3,59 +3,46 @@ package com.example.compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
+            val painter = painterResource(id = R.drawable.android)
+            val desc = "Android logo"
+            val title = "Hello there!"
+            Box(
                 modifier = Modifier
-                    .background(Color.Cyan)
-                    .fillMaxHeight(0.5f)
-//                    .width(100.dp)     // custom width
-//                    .requiredWidth(600.dp)    // gadis screens garet
-                    .fillMaxWidth()
-//                    .padding(20.dp)
-                    .border(10.dp, Color.Blue, shape = RectangleShape)
-                    .padding(10.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-                    .border(10.dp, Color.Blue)                  // yuradgeba miaqcie orders
-                    .padding(10.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-
-
+                    .fillMaxWidth(0.5f)
+                    .padding(16.dp)
             ) {
-                Text(
-                    text = "Hello", modifier = Modifier.clickable {
-                        // click event
-                    }
-                )     // top left paddings  // do not have margins / diff - offsetit gadaefareba ertmanets(ashorebs mxolod sakutari kidedan). margin ashorebda ertmanetisgan
-                Spacer(modifier = Modifier.height(60.dp))  // just space
-                Text(text = "Android!", modifier = Modifier.offset(20.dp))
+                ImageCard(painter = painter, contentDescription = desc, title = title)
+
             }
-
-
         }
     }
 }
@@ -67,5 +54,47 @@ fun GreetingPreview() {
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Text(text = "hello")
         Text(text = "Android")
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter, contentDescription: String, title: String, modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)   // card elevation
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            // image
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            // gradient
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent, Color.Black
+                            ), startY = 400f
+
+                        )
+                    )
+            )
+            // text box
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .padding(12.dp), contentAlignment = Alignment.BottomCenter
+
+            ) {
+                Text(text = title, style = TextStyle(color = Color.Green, fontSize = 16.sp))
+            }
+        }
     }
 }
