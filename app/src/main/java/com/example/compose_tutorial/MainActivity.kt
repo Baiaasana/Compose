75b204ec1d81d6173b15e4a65055b8eda9c35af2
b.asanidze <b.asanidze@mm.ge>
2023-11-15 15:12:13 +0400

    buildAnnotatedString

--- app/src/main/java/com/example/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/compose_tutorial/MainActivity.kt
@@ -3,27 +3,25 @@ package com.example.compose_tutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -31,70 +29,59 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val fontFamily = FontFamily(
+            Font(R.font.lexend_black, FontWeight.Black),
+            Font(R.font.lexend_bold, FontWeight.Bold),
+            Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
+            Font(R.font.lexend_extralight, FontWeight.ExtraLight),
+            Font(R.font.lexend_light, FontWeight.Light),
+            Font(R.font.lexend_medium, FontWeight.Medium),
+            Font(R.font.lexend_regular, FontWeight.Normal),
+            Font(R.font.lexend_semibold, FontWeight.SemiBold),
+            Font(R.font.lexend_thin, FontWeight.Thin),
+        )
+
         setContent {
-            val painter = painterResource(id = R.drawable.android)
-            val desc = "Android logo"
-            val title = "Hello there!"
             Box(
                 modifier = Modifier
-                    .fillMaxWidth(0.5f)
-                    .padding(16.dp)
+                    .fillMaxSize()
+                    .background(Color.Black)
+                    .offset(y = 30.dp)
             ) {
-                ImageCard(painter = painter, contentDescription = desc, title = title)
-
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append("J")
+                        }
+                        append("etpack")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ) {
+                            append(" C")
+                        }
+                        append("ompose")
+                    },
+                    Modifier.fillMaxSize(),
+                    color = Color.White,
+                    fontSize = 28.sp,
+                    fontFamily = fontFamily,
+                    fontStyle = FontStyle.Italic,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
+                )
             }
         }
     }
 }
 
 
-@Preview(showBackground = true, showSystemUi = false)
-@Composable
-fun GreetingPreview() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Text(text = "hello")
-        Text(text = "Android")
-    }
-}
-
-@Composable
-fun ImageCard(
-    painter: Painter, contentDescription: String, title: String, modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)   // card elevation
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            // image
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-            // gradient
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent, Color.Black
-                            ), startY = 400f
-
-                        )
-                    )
-            )
-            // text box
-            Box(
-                Modifier
-                    .fillMaxSize()
-                    .padding(12.dp), contentAlignment = Alignment.BottomCenter
-
-            ) {
-                Text(text = title, style = TextStyle(color = Color.Green, fontSize = 16.sp))
-            }
-        }
-    }
-}
