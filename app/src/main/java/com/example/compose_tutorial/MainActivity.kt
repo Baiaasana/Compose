05c202d31ad997507c7fbdb72de589975065ac27
b.asanidze <b.asanidze@mm.ge>
2023-11-20 13:36:32 +0400

    state / remember / recomposing

--- app/src/main/java/com/example/compose_tutorial/MainActivity.kt
+++ app/src/main/java/com/example/compose_tutorial/MainActivity.kt
@@ -1,87 +1,79 @@
 package com.example.compose_tutorial
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val fontFamily = FontFamily(
-            Font(R.font.lexend_black, FontWeight.Black),
-            Font(R.font.lexend_bold, FontWeight.Bold),
-            Font(R.font.lexend_extrabold, FontWeight.ExtraBold),
-            Font(R.font.lexend_extralight, FontWeight.ExtraLight),
-            Font(R.font.lexend_light, FontWeight.Light),
-            Font(R.font.lexend_medium, FontWeight.Medium),
-            Font(R.font.lexend_regular, FontWeight.Normal),
-            Font(R.font.lexend_semibold, FontWeight.SemiBold),
-            Font(R.font.lexend_thin, FontWeight.Thin),
-        )
-
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color.Black)
-                    .offset(y = 30.dp)
-            ) {
-                Text(
-                    buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ) {
-                            append("J")
-                        }
-                        append("etpack")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 50.sp
-                            )
-                        ) {
-                            append(" C")
-                        }
-                        append("ompose")
-                    },
-                    Modifier.fillMaxSize(),
-                    color = Color.White,
-                    fontSize = 28.sp,
-                    fontFamily = fontFamily,
-                    fontStyle = FontStyle.Italic,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Center,
-                    textDecoration = TextDecoration.Underline
+
+            Column(modifier = Modifier.fillMaxSize()) {
+                // at recomposition it does not update state, but remember initial value
+
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColoredBox(
+                    modifier = Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    Log.d("recompose", it.toString())
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
                 )
+
             }
         }
     }
 }
 
+// recomposition  - call the composable function again with new data , redraw widgets
+// Compose is declarative and as such the only way to update it is by calling the same composable with new arguments.
+//
+//Composition: a description of the UI built by Jetpack Compose when it executes composables.
+//
+//Initial composition: creation of a Composition by running composables the first time.
+//
+//Recomposition: re-running composables to update the Composition when data changes.
+
+@Composable
+fun ColoredBox(
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+) {
+
+    Box(modifier = modifier
+        .background(Color.Red)
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
+        }
+    )
+}
 
